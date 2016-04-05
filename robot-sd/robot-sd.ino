c8b16f9f0486f4a9bf0bdca838117472bbc8c68c
Mark Hahn <mark@hahnca.com>
2016-04-04 18:38:53 -0700

    sd raw fits in robot

--- robot-sd/robot-sd.ino
+++ robot-sd/robot-sd.ino
@@ -6,6 +6,8 @@
 #include <SD.h>
 #include "utility/debug.h"
 #include "utility/socket.h"
+#include "Sd2Card.h"
+
 
 ///////////////////// DEF MISC ////////////////////
 #define LOOP_FREQ 10 // ms
@@ -25,9 +27,14 @@ float mini (int a, int b) {
 
 ///////////////////// DEF SD ////////////////////
 Sd2Card card;
-SdVolume volume;
-SdFile root;
-const int chipSelect = 4;    
+uint8_t block[128];
+
+void printErr() {
+  Serial.print(card.errorCode());
+  Serial.print(" "); 
+  Serial.println(card.errorData());
+  while(1) delay(1000); 
+}
 
 
 ///////////////////// DEF LED ////////////////////
@@ -161,71 +168,16 @@ void setup() {
 
 
   //////////////////// SETUP SD ////////////////////////
-  Serial.print("\nInitializing SD card...");
-  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
-  // Note that even if it's not used as the CS pin, the hardware SS pin 
-  // (10 on most Arduino boards, 53 on the Mega) must be left as an output 
-  // or the SD library functions will not work. 
-  pinMode(SS, OUTPUT);
-
-
-  // we'll use the initialization code from the utility libraries
-  // since we're just testing if the card is working!
-  while (!card.init(SPI_HALF_SPEED, chipSelect)) {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card is inserted?");
-    Serial.println("* Is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-  } 
+  if (!card.init(SPI_HALF_SPEED, 4)) { Serial.print("Card Init Err: "); printErr(); }
+  Serial.print("Card initialized, size: "); 
+  Serial.print(card.cardSize()*512.0/1e9);
+  Serial.println(" GB");
   
-  // print the type of card
-  Serial.print("\nCard type: ");
-  switch(card.type()) {
-    case SD_CARD_TYPE_SD1:
-      Serial.println("SD1");
-      break;
-    case SD_CARD_TYPE_SD2:
-      Serial.println("SD2");
-      break;
-    case SD_CARD_TYPE_SDHC:
-      Serial.println("SDHC");
-      break;
-    default:
-      Serial.println("Unknown");
-  }
-
-  // Now we will try to open the 'volume'/'partition' - it should be FAT16 or FAT32
-  if (!volume.init(card)) {
-    Serial.println("Could not find FAT16/FAT32 partition.\nMake sure you've formatted the card");
-    return;
-  }
-
-
-  // print the type and size of the first FAT-type volume
-  uint32_t volumesize;
-  Serial.print("\nVolume type is FAT");
-  Serial.println(volume.fatType(), DEC);
-  Serial.println();
+  uint32_t start = micros();
+  if (!card.readData(1234, 256, 128, block)) { Serial.print("Card Read Err: "); printErr(); }
+  uint32_t end = micros();
+  Serial.println(end-start);
   
-  volumesize = volume.blocksPerCluster();    // clusters are collections of blocks
-  volumesize *= volume.clusterCount();       // we'll have a lot of clusters
-  volumesize *= 512;                            // SD card blocks are always 512 bytes
-  Serial.print("Volume size (bytes): ");
-  Serial.println(volumesize);
-  Serial.print("Volume size (Kbytes): ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-  Serial.print("Volume size (Mbytes): ");
-  volumesize /= 1024;
-  Serial.println(volumesize);
-
-  
-  Serial.println("\nFiles found on the card (name, date and size in bytes): ");
-  root.openRoot(volume);
-  
-  // list all files in the card with date and size
-  root.ls(LS_R | LS_DATE | LS_SIZE);
-
 
   //////////////////// SETUP BATT ////////////////////////
   chkVbatt();
--- sd-raw/sd-raw.ino
+++ sd-raw/sd-raw.ino
@@ -1,10 +1,30 @@
 
 #include <SPI.h>
+#include "Sd2Card.h"
 
+Sd2Card card;
+uint8_t block[512];
+
+void printErr() {
+  Serial.print(card.errorCode());
+  Serial.print(" "); 
+  Serial.println(card.errorData());
+  while(1) delay(1000); 
+}
 
 void setup() {
-  /* Implementation of class used to create `SDCard` object. */
-  card.init(SPI_HALF_SPEED, 4, 11, 12, 13);
+  Serial.begin(115200);
+  
+  if (!card.init(SPI_HALF_SPEED, 4)) { Serial.print("Card Init Err: "); printErr(); }
+  Serial.print("Card initialized, size: "); 
+  Serial.print(card.cardSize()*512.0/1e9);
+  Serial.println(" GB");
+
+  if (!card.readBlock(-1, block)) { Serial.print("Card Read Err: "); printErr(); }
+  // int i;
+  // for (i = 0; i < 512; i++) {
+  //   Serial.println(block[i]);
+  // }
   
 }
 
