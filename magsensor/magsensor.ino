d45772c0a40706f3a9ea495594377b244bc1858f
Mark Hahn <mark@hahnca.com>
2016-04-08 23:38:31 -0700

    wifi and compass work on mega

--- magsensor/magsensor.ino
+++ magsensor/magsensor.ino
@@ -1,8 +1,8 @@
+#include <math.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
 
-/* Assign a unique ID to this sensor at the same time */
 Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
 
 void displaySensorDetails(void)
@@ -29,39 +29,38 @@ void setup(void)
   Serial.begin(115200);
   Serial.println("Magnetometer Test"); Serial.println("");
   
-  /* Enable auto-gain */
+  mag.setMagRate(LSM303_MAGRATE_220);
   mag.enableAutoRange(true);
-  
-  /* Initialise the sensor */
   if(!mag.begin())
   {
-    /* There was a problem detecting the LSM303 ... check your connections */
     Serial.println("Ooops, no LSM303 detected ... Check your wiring!");
     while(1);
   }
-
   TWSR = 0;
-  TWBR = 2;
-  // 12 => 736
-  //  2 => 570
-  
-  /* Display some basic information on this sensor */
+  TWBR = 2;  // 12 => 736, 2 => 570
   displaySensorDetails();
 }
 
 void loop(void) 
 {
-  /* Get a new sensor event */ 
   sensors_event_t event; 
   
-  uint16_t start = micros();  
+  // uint16_t start = micros();  
   mag.getEvent(&event);
-  uint16_t end = micros();  
-  Serial.println(end-start);
+  // uint16_t end = micros();  
+  // Serial.println(end-start);
+
+  // r = √ ( x*x + y*y )
+  // θ = arctan ( y / x )
+  
+  double degrees = 360.0 * (-atan2(event.magnetic.y, event.magnetic.z) / (2*M_PI));
+  while (degrees  <   0.0) degrees += 360.0;
+  while (degrees >= 360.0) degrees -= 360.0;
+  Serial.print("heading: "); Serial.println(degrees);
  
   /* Display the results (magnetic vector values are in micro-Tesla (uT)) */
-  Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
-  Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
-  Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("uT");
+  // Serial.print("X: "); Serial.print(event.magnetic.x); Serial.print("  ");
+  // Serial.print("Y: "); Serial.print(event.magnetic.y); Serial.print("  ");
+  // Serial.print("Z: "); Serial.print(event.magnetic.z); Serial.print("  ");Serial.println("uT");
   delay(500);
 }
---
+++ wifi-compass/wifi-compass.ino
@@ -0,0 +1,174 @@
+#include <SPI.h>
+#include <math.h>
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_CC3000.h>
+#include <Adafruit_LSM303_U.h>
+#include "utility/debug.h"
+#include "utility/socket.h"
+
+//////////////////// WIFI DEFINE ///////////////////
+
+#define PORT           23
+#define CC3000_CS      10
+#define CC3000_IRQ      3
+#define CC3000_VBAT     5
+#define WLAN_SSID     "hahn-fi"
+#define WLAN_PASS     "NBVcvbasd987"
+#define WLAN_SECURITY  WLAN_SEC_WPA2
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(CC3000_CS,   CC3000_IRQ, 
+                                         CC3000_VBAT, SPI_CLOCK_DIVIDER);
+Adafruit_CC3000_Server server(PORT);
+
+bool displayConnectionDetails(void)
+{
+  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
+  if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
+  {
+    Serial.println("Unable to retrieve the IP Address!\r\n");
+    return false;
+  }
+  else
+  {
+    Serial.print("\r\nIP Addr: "); cc3000.printIPdotsRev(ipAddress);
+    Serial.print("\r\nNetmask: "); cc3000.printIPdotsRev(netmask);
+    Serial.print("\r\nGateway: "); cc3000.printIPdotsRev(gateway);
+    Serial.print("\r\nDHCPsrv: "); cc3000.printIPdotsRev(dhcpserv);
+    Serial.print("\r\nDNSserv: "); cc3000.printIPdotsRev(dnsserv);
+    Serial.println('\r');
+    return true;
+  }
+}
+
+//////////////////// COMPASS DEFINE ///////////////////
+
+Adafruit_LSM303_Mag_Unified mag = Adafruit_LSM303_Mag_Unified(12345);
+
+void displaySensorDetails(void)
+{
+  sensor_t sensor;
+  mag.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" uT");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" uT");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" uT");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+//////////////////// SETUP ////////////////////////
+
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println(F("Starting wifi-compass ...\n")); 
+  Serial.print("Free RAM: "); Serial.println(getFreeRam(), DEC);
+  
+  
+  //////////////////// WIFI SETUP ///////////////////
+
+  Serial.println("\nInitializing wifi ...");
+
+  if (!cc3000.begin()) {
+    Serial.println("Couldn't begin(! Check your wiring?");
+    while(1);
+  }
+  Serial.println("Connecting to access point...");
+  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+    Serial.println("Failed!");
+    while(1);
+  }
+  Serial.println("Connected!");
+  
+  Serial.println("Request DHCP");
+  while (!cc3000.checkDHCP()) {
+    delay(100); // ToDo: Insert a DHCP timeout!
+  }  
+  /* Display the IP address DNS, Gateway, etc. */  
+  while (! displayConnectionDetails()) {
+    delay(1000);
+  }
+  // Start listening for connections
+  server.begin();
+  Serial.println("Listening for clients ...\n");
+  
+//////////////////// COMPASS SETUP ///////////////////
+
+    mag.setMagRate(LSM303_MAGRATE_220);
+    mag.enableAutoRange(true);
+    if(!mag.begin()) {
+      Serial.println("no magnometer detected");
+      while(1);
+    }
+    TWSR = 0;
+    TWBR = 2;  // 12 => 736, 2 => 570
+    displaySensorDetails();
+}
+
+//////////////////// LOOP ////////////////////////
+
+void loop(void) {
+  double heading;
+
+//////////////////// COMPASS LOOP ////////////////////////
+
+  sensors_event_t event; 
+  mag.getEvent(&event);
+  // θ = arctan ( y / x )
+  heading = 360.0 * (-atan2(event.magnetic.y, event.magnetic.z) / (2*M_PI));
+  while (heading  <   0.0) heading += 360.0;
+  while (heading >= 360.0) heading -= 360.0;
+
+//////////////////// WIFI LOOP ////////////////////////
+
+  // static char headingStr[6];
+  // headingStr[3] = '\r';
+  // headingStr[4] = 0;
+  // headingStr[5] = 0;
+  // static bool connected = 0;
+  // static int clientIdx = 0;
+  bool newClient = false;
+  int clientIdx;
+  
+  if(server.availableIndex(&newClient) > -1) {
+    for (clientIdx=0; clientIdx<3; clientIdx++) {
+      Adafruit_CC3000_ClientRef client = server.getClientRef(clientIdx);
+      Serial.print("idx: "); Serial.print(clientIdx); 
+      Serial.print(", connected: "); Serial.println(client.connected()); 
+      if (client.connected())
+        client.fastrprintln("hello client");
+      // Serial.print(": "); Server.println(); 
+    }
+  }
+  // if (!clientIdx) clientIdx = server.availableIndex(&newClient);
+  // Serial.println(server.availableIndex(&newClient));
+  
+  // if (clientIdx) {
+  //    client = server.available();
+  //   if (newClient) {
+  //     Serial.print("client connected: "); Serial.println(clientIdx);
+  //     connected = true;
+  //   }
+  //   if (client.available() && client.read() == 'q') {
+  //     client.close();
+  //   } else {
+  //     uint16_t int_heading = round(heading);
+  //     for (i = 0; i < 3; i++) {
+  //       headingStr[3-i] = '0' + int_heading % 10;
+  //       int_heading /= 10;
+  //     }
+  //     client.write(headingStr, 6);
+  //     Serial.println(headingStr);
+  //   }
+  // } else {
+  //   if (!client.connected) {
+  //     Serial.println("client disconnected");
+  //     connected = false;
+  //   }
+  // }
+  delay(500);
+}
